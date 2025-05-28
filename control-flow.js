624fe5362b7b4b73f616ffb2fa374fb2d4dc758d
Gachuhimartin <martingachuhimaina@gmail.com>
2025-05-28 16:32:04 +0300

    If statement

---
+++ control-flow.js
@@ -0,0 +1,21 @@
+//let priceOfChocolate=1.99;
+//let hasAmountInCash=0.99;
+
+//if(hasAmountInCash>=priceOfChocolate){
+ //   console.log('Enjoy your purchase');
+//}
+//else{
+//  console.log('Sorry you do not have enough')
+//}
+
+let hour =13;
+
+if(hour >=6 && hour <= 12)
+    console.log('Serving breakfast')
+
+else if(hour>= 12 && hour<=14)
+    console.log('Serving lunch')
+
+
+else
+    console.log('Serving dinner')
--- operators.js
+++ operators.js
@@ -7,24 +7,33 @@
 //const canDrive=age >= 16 ? true :false;
 //console.log(canDrive);
 
-let points=110;
-const customerType= points > 100 ? 'gold' : 'silver';
+//let points=110;
+//const customerType= points > 100 ? 'gold' : 'silver';
 
-console.log(customerType);
+//console.log(customerType);
 
 //Logical operators
 //|| or operator
 //&& and operator
 //! operator
 
-let isClosed=true;
-const me=!isClosed;
+//let isClosed=true;
+//const me=!isClosed;
 
-console.log(me);
+//console.log(me);
 
 
 //?? null coalescing is  a short vesion of (a!== null && a !== undefined) ? a : false;
-let a =null;
-const result=a ?? false;
+//let a =null;
+//const result=a ?? false;
 
-console.log(result);
+//console.log(result);
+
+//LOGICAL OPERATORS WITH NON-BOOLEANS
+//let usersChosenColor='blue';
+//let defaultColor='green';
+
+//const currentWebsiteColor = usersChosenColor || defaultColor;
+//console.log(currentWebsiteColor);
+
+//OPERATOR PRECEDENCE(PEMDAS) precedence can be overide using parenthesis
