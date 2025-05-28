d988fd807aa87da75b3b5a49ed871606e98a9d45
Gachuhimartin <martingachuhimaina@gmail.com>
2025-05-28 14:40:13 +0300

    Objects and variables in java script

---
+++ objects.js
@@ -0,0 +1,26 @@
+let course={
+    name:'Martin',
+    hours:3
+};
+//Acessing value using the . method or update
+//console.log(course.name);
+
+//course.name='Javascript Fundamentals';
+
+//console.log(course.name);
+
+//Using the bracket method to access or update the value
+
+//Use this access method when you don't know which property you want to access till runtime
+
+//console.log(course['name']);
+
+//course['name']='JavaScript 101';
+//console.log(course['name']);
+
+//Example
+let  property='hours';
+
+console.log(course[property]);
+
+
---
+++ variables.js
@@ -0,0 +1,38 @@
+let message='Hello';
+message='World!';
+
+const christmas_2024='12.15.2024';
+console.log(christmas_2024)
+
+let favoriteFruit='strawberry';
+let favoriteIceCream="chocolate";
+let favouriteProgrammingLanguage='JavaScript';
+
+let numberOfDounuts=12;
+let pi=3.14;
+
+let veryLargeNumber=5467366337463746n;
+
+let lovesCoding=true;
+
+let favoriteColor;
+
+favoriteFruit=null;
+
+const uniqueKey=Symbol();
+
+
+let course ={
+    name:'Javascript for beginners',
+    hours:3
+};
+
+let firstName='Steven';
+console.log(typeof firstName);
+
+firstName=100;
+console.log(typeof firstName);
+
+
+firstName=true;
+console.log(typeof firstName);
