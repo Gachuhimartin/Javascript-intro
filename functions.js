706ab39ad627d904faac3968796a16bc83e33c3b
Gachuhimartin <martingachuhimaina@gmail.com>
2025-05-28 15:48:21 +0300

    Operators

--- functions.js
+++ functions.js
@@ -4,8 +4,21 @@
 
 //sayHi();
 
-function sayHi(name){
-console.log('Hi! '+ name);
+//function sayHi(name){
+//console.log('Hi! '+ name);
+//}
+
+//sayHi('Steven');
+
+
+//function multiply(num1,num2){
+//    return num1 * num2;
+//}
+
+//console.log(multiply(2,2));
+
+function multiply(num1,num2){
+    const product= num1 * num2;
 }
 
-sayHi('Steven');
+console.log(multiply(2,2));
---
+++ operators.js
@@ -0,0 +1,30 @@
+//Assignmet operator
+//Comparison operator
+//Logical operator
+//Equality operatorl
+//Tenary oerator
+//let age=16;
+//const canDrive=age >= 16 ? true :false;
+//console.log(canDrive);
+
+let points=110;
+const customerType= points > 100 ? 'gold' : 'silver';
+
+console.log(customerType);
+
+//Logical operators
+//|| or operator
+//&& and operator
+//! operator
+
+let isClosed=true;
+const me=!isClosed;
+
+console.log(me);
+
+
+//?? null coalescing is  a short vesion of (a!== null && a !== undefined) ? a : false;
+let a =null;
+const result=a ?? false;
+
+console.log(result);
